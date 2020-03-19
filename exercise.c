cfd3660c513100d72d76ba39174c6276b96add41
Alberto Parravicini <albyratta@gmail.com>
2020-03-19 13:38:17 +0100

    Update exercise.c

--- exercise.c
+++ exercise.c
@@ -1,5 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<stdlib.h>
+
+#define N 10
 
 int main(int argc, char *argv[]) {
-  // The solution of the exercise goes here
+    int array[N] = { 0 };
+    int array_sum = 0;
+
+    // Read array from input;
+    int input_size = argc - 1;
+    if (input_size > N || input_size <= 0) {
+        printf("figa ma sei scemo, N=%d\n", input_size);
+        return -1;
+    }
+    for (int i = 0; i < input_size; i++) {
+        array[i] = atoi(argv[i + 1]);
+    }
+
+    ////////////////////////////
+    // Add your function here //
+    ////////////////////////////
+  
+  
+
+    ////////////////////////////
+
+    printf("somma del vettore=%d\n", array_sum);
+
+    return 0;
 }
